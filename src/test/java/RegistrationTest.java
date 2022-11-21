1f63ece230eb44f938dfb0ec962b0dd75b91ef58
Maria Manyk <mariiamanyk@gmail.com>
2022-11-22 02:06:17 +0300

    Added RegistrationTest

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,16 +1,14 @@
-package Tests;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.BeforeAll;
+        import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+        import static com.codeborne.selenide.Condition.appear;
+        import static com.codeborne.selenide.Condition.text;
+        import static com.codeborne.selenide.Selectors.byText;
+        import static com.codeborne.selenide.Selenide.*;
 
-
-public class RegistrationTests {
+public class RegistrationTest {
 
     @BeforeAll
     static void beforeAll() {
@@ -20,8 +18,7 @@ public class RegistrationTests {
 
     @Test
     void successfulRegistrationTest() {
-        String userName = "Alex";
-
+        String userName = "Mariia";
 
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
@@ -29,17 +26,16 @@ public class RegistrationTests {
         executeJavaScript("$('footer').remove()");
 
         $("#firstName").setValue(userName);
-        $("#lastName").setValue("Egorov");
-        $("#userEmail").setValue("alex@egorov.com");
+        $("#lastName").setValue("Egorova");
+        $("#userEmail").setValue("maria@egorova.com");
         $("#genterWrapper").$(byText("Other")).click(); // best
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
-        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__year-select").selectOption("2006");
         $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("img/1.png");
         $("#currentAddress").setValue("Some address 1");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
@@ -49,7 +45,7 @@ public class RegistrationTests {
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(userName), text("Egorov"),
-                text("alex@egorov.com"), text("1234567890"));
+        $(".table-responsive").shouldHave(text(userName), text("Egorova"),
+                text("maria@egorova.com"), text("1234567890"));
     }
 }
